c6bf890e8af8203a7a25880219ebaa274ba9d459
Igor Cotrim <igorcotrim.dev@gmail.com>
2023-09-14 19:57:35 -0300

    feat: :sparkles: add config to deploy in Render

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL); //Getting the URL from the Environment
+
 module.exports = ({ env }) => {
   const client = env("DATABASE_CLIENT", "postgres");
 
@@ -29,44 +32,41 @@ module.exports = ({ env }) => {
         max: env.int("DATABASE_POOL_MAX", 10),
       },
     },
-    mysql2: {
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "0.0.0.0"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
+        // host: env("DATABASE_HOST", "0.0.0.0"),
+        // port: env.int("DATABASE_PORT", 5432),
+        // database: env("DATABASE_NAME", "strapi"),
+        // user: env("DATABASE_USERNAME", "strapi"),
+        // password: env("DATABASE_PASSWORD", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
+        host: !!process.env.DATABASE_URL
+          ? env("DATABASE_HOST", "0.0.0.0")
+          : config.host,
+        port: !!process.env.DATABASE_URL
+          ? env.int("DATABASE_PORT", 5432)
+          : config.port,
+        database: !!process.env.DATABASE_URL
+          ? env("DATABASE_NAME", "strapi")
+          : config.database,
+        user: !!process.env.DATABASE_URL
+          ? env("DATABASE_USERNAME", "strapi")
+          : config.user,
+        password: !!process.env.DATABASE_URL
+          ? env("DATABASE_PASSWORD", "strapi")
+          : config.password,
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
           ca: env("DATABASE_SSL_CA", undefined),
           capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          // cipher: env("DATABASE_SSL_CIPHER", undefined),
+          cipher: env(
+            "DATABASE_SSL_CIPHER",
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+          ),
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
             true
