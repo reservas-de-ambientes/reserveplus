0245526fe44dd0119f8a4285fee128e3b64c59bf
Igor Cotrim <igorcotrim.dev@gmail.com>
2023-09-15 14:07:06 -0300

    fix: test routes in prod

--- client/next.config.js
+++ client/next.config.js
@@ -8,30 +8,37 @@ const nextConfig = {
     {
       source: "/ambientes",
       destination: "/pages/Ambiences",
+      permanent: true,
     },
     {
       source: "/reservar",
       destination: "/pages/Reserve",
+      permanent: true,
     },
     {
       source: "/usuarios",
       destination: "/pages/Users",
+      permanent: true,
     },
     {
       source: "/solicitacoes",
       destination: "/pages/Solicitations",
+      permanent: true,
     },
     {
       source: "/semestres",
       destination: "/pages/Semesters",
+      permanent: true,
     },
     {
       source: "/usuarios",
       destination: "/pages/Users",
+      permanent: true,
     },
     {
       source: "/signIn",
       destination: "/pages/SignIn",
+      permanent: true,
     },
   ],
 };
