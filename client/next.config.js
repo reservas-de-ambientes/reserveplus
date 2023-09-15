146e268cf42020045bd9c1431b665624b6daf99b
Igor Cotrim <igorcotrim.dev@gmail.com>
2023-09-15 13:56:41 -0300

    fix: :bug: change rewrites with redirects

--- client/next.config.js
+++ client/next.config.js
@@ -4,34 +4,41 @@ const nextConfig = {
     appDir: true,
   },
   output: "standalone",
-  rewrites: async () => [
+  redirects: async () => [
     {
       source: "/ambientes",
       destination: "/pages/Ambiences",
+      permanent: true,
     },
     {
       source: "/reservar",
       destination: "/pages/Reserve",
+      permanent: true,
     },
     {
       source: "/usuarios",
       destination: "/pages/Users",
+      permanent: true,
     },
     {
       source: "/solicitacoes",
       destination: "/pages/Solicitations",
+      permanent: true,
     },
     {
       source: "/semestres",
       destination: "/pages/Semesters",
+      permanent: true,
     },
     {
       source: "/usuarios",
       destination: "/pages/Users",
+      permanent: true,
     },
     {
       source: "/signIn",
       destination: "/pages/SignIn",
+      permanent: true,
     },
   ],
 };
