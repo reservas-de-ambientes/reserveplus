a3d3050fb9f1cac1cd4d8c8953e54f25765046dd
Igor Cotrim <igorcotrim.dev@gmail.com>
2023-09-14 22:25:37 -0300

    fix: :bug: error in deploy config

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,5 @@
 const path = require("path");
 
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL); //Getting the URL from the Environment
-
 module.exports = ({ env }) => {
   const client = env("DATABASE_CLIENT", "postgres");
 
@@ -34,27 +31,18 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        // connectionString: env("DATABASE_URL"),
-        // host: env("DATABASE_HOST", "0.0.0.0"),
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "0.0.0.0"),
         port: env.int("DATABASE_PORT", 5432),
-        // database: env("DATABASE_NAME", "strapi"),
-        // user: env("DATABASE_USERNAME", "strapi"),
-        // password: env("DATABASE_PASSWORD", "strapi"),
-        host: config.host,
-        // port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
           ca: env("DATABASE_SSL_CA", undefined),
           capath: env("DATABASE_SSL_CAPATH", undefined),
-          // cipher: env("DATABASE_SSL_CIPHER", undefined),
-          cipher: env(
-            "DATABASE_SSL_CIPHER",
-            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-          ),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
             true
@@ -79,7 +67,7 @@ module.exports = ({ env }) => {
     },
   };
 
-  console.log("DATABASE_URL", env("DATABASE_URL"), connections[client], config);
+  console.log("DATABASE_URL", env("DATABASE_URL"), connections[client]);
 
   return {
     connection: {
